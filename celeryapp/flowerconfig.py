2ba075c3c5387adc210120aeece6dbce12b38ae9
nikslt <nikhil.parihar@letstransport.team>
2024-02-28 17:44:00 +0530

    changing only the desired one

--- celeryapp/flowerconfig.py
+++
@@ -1,6 +0,0 @@
-import config
-
-persistent = True
-db = config.FLOWER_DB_PATH
-
-basic_auth = [f"{config.FLOWER_USER}:{config.FLOWER_PASSWORD}"]
--- common_deployements/helmdeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prints each command before execution
-set -o xtrace
-
-helm upgrade --install \
-    ${JOB_NAME} ./build/helm/celery-helm \
-    -f build/${DEPLOYMENT_ENV}.values.yaml \
-    --set-string "image.tag=${BUILD_NUMBER}"
